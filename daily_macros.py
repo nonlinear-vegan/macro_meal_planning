305f980dffa484615d4256c8eb23642ace3217df
MARohrdanz <marohrdanz@mdanderson.org>
2017-02-24 20:20:03 -0600

    25 feb plan

--- daily_macros.py
+++ daily_macros.py
@@ -4,35 +4,41 @@ import json
 
 def get_ingredient_macros(ingredient):
 	""" macros for each ingredient quanity given amount field"""
-	eggplant      = {'cal':25,'fat':0.2,'carbs':6,'prot':1,'amount':'100g'}
-	cherry_tomato = {'cal':18,'fat':0.2,'carbs':3.9,'prot':0.9,'amount':'100g'}
-	tomato_red    = {'cal':32,'fat':0,'carbs':7,'prot':2,'amount':'1cupchopped'}
-	olives        = {'cal':4000/15.,'fat':400/15.,'carbs':100./15.,'prot':0,'amount':'100g'}
-	westsoy_tofu  = {'cal':237,'fat':13,'carbs':7.5,'prot':25,'amount':'1/2brick'}
-	nigari_tofu   = {'cal':70,'fat':4.2,'carbs':1.7,'prot':8.2,'amount':'100g'}
-	cauliflower   = {'cal':25,'fat':0.3,'carbs':5,'prot':1.9,'amount':'100g'}
-	onion         = {'cal':40,'fat':0.1,'carbs':9,'prot':1.1,'amount':'100g'}
-	summer_squash = {'cal':16,'fat':0.2,'carbs':3.4,'prot':1.2,'amount':'100g'}
-	beets         = {'cal':43,'fat':0.2,'carbs':10,'prot':1.6,'amount':'100g'}
-	carrots       = {'cal':41,'fat':0.4,'carbs':10,'prot':0.9,'amount':'100g'}
-	performance_protein= {'cal':160,'fat':3,'carbs':6,'prot':30,'amount':'1scoop'} 
-	vega_smoothie      = {'cal':90,'fat':0.5,'carbs':5,'prot':15,'amount':'1scoop'}
-	flax_oil           = {'cal':40, 'fat':5,'carbs':0,'prot':0,'amount':'1tsp'}  
-	nutritional_yeast  = {'cal':20,'fat':0,'carbs':2,'prot':3,'amount':'1T'}    
-	romain_lettuce     = {'cal':17,'fat':0,'carbs':3,'prot':1,'amount':'100g'}  
-	green_leaf_lettuce = {'cal':15,'fat':0,'carbs':3,'prot':1,'amount':'100g'}
-	green_beans        = {'cal':15,'fat':0,'carbs':3.5,'prot':1,'amount':'50g'}
-	pinto_beans        = {'cal':60,'fat':0,'carbs':22,'prot':7,'amount':'1/4drycup'}  
+	### fats
+	flax_oil           = {'cal':40, 'fat':5,   'carbs':0, 'prot':0, 'amount':'1tsp'}  
+	coconut_oil        = {'cal':20, 'fat':2.3, 'carbs':0, 'prot':0, 'amount':'1/2teasp'}
+	### proteins
+	westsoy_tofu        = {'cal':237, 'fat':13,  'carbs':7.5, 'prot':25,  'amount':'1/2brick'}
+	nigari_tofu         = {'cal':70,  'fat':4.2, 'carbs':1.7, 'prot':8.2, 'amount':'100g'}
+	banyan_tofu         = {'cal':225, 'fat':10,  'carbs':7.5, 'prot':25,  'amount':'1/2package'}
+	performance_protein = {'cal':160, 'fat':3,   'carbs':6,   'prot':30,  'amount':'1scoop'} 
+	vega_smoothie       = {'cal':90,  'fat':0.5, 'carbs':5,   'prot':15,  'amount':'1scoop'}
+	now_pea_protein     = {'cal':120, 'fat':2,   'carbs':1,   'prot':24,  'amount':'1levelscoop'}
+	nutritional_yeast   = {'cal':20,  'fat':0,   'carbs':2,   'prot':3,   'amount':'1T'}    
+	### carbs
+	brown_rice_dry     = {'cal':171,'fat':1,  'carbs':36, 'prot':4, 'amount':'1/4cupdry'}
+	rolled_oats        = {'cal':160,'fat':2.5,'carbs':27, 'prot':7, 'amount':'1/2cupdry'}
+	sweet_potato       = {'cal':86, 'fat':0,  'carbs':20, 'prot':2, 'amount':'100g'}
+	acorn_squash       = {'cal':40, 'fat':0,  'carbs':10, 'prot':1, 'amount':'100g'}   
+	## sugar carbs
 	coconut_water_MP   = {'cal':60,'fat':0,'carbs':15,'prot':0,'amount':'1boxserving'} 
-	coconut_oil        = {'cal':20,'fat':2.3,'carbs':0,'prot':0,'amount':'1/2teasp'}
 	orange             = {'cal':69,'fat':0,'carbs':18,'prot':1,'amount':'1orange'}
-	acorn_squash       = {'cal':40,'fat':0,'carbs':10,'prot':1,'amount':'100g'}   
-	sweet_potato       = {'cal':86,'fat':0,'carbs':20,'prot':2,'amount':'100g'}
-	brown_rice_dry     = {'cal':685./4,'fat':5./4,'carbs':143./4,'prot':15./4,'amount':'1/4cupdry'}
-	rolled_oats        = {'cal':150,'fat':3,'carbs':27,'prot':5,'amount':'1/2cupdry'}
-	pea_pods           = {'cal':42,'fat':0.2,'carbs':7.8,'prot':2.8,'amount':'100g'}
-	red_pepper_bell    = {'cal':15,'fat':0,'carbs':3,'prot':0,'amount':'50g'}
-	now_pea_protein    = {'cal':120,'fat':2,'carbs':1,'prot':24,'amount':'1levelscoop'}
+	### veggies
+	onion         = {'cal':40, 'fat':0.1, 'carbs':9,   'prot':1.1, 'amount':'100g'}
+	summer_squash = {'cal':16, 'fat':0.2, 'carbs':3.4, 'prot':1.2, 'amount':'100g'}
+	beets         = {'cal':43, 'fat':0.2, 'carbs':10,  'prot':1.6, 'amount':'100g'}
+	carrots       = {'cal':41, 'fat':0.4, 'carbs':10,  'prot':0.9, 'amount':'100g'}
+	cauliflower   = {'cal':25, 'fat':0.3, 'carbs':5,   'prot':1.9, 'amount':'100g'}
+	### other 
+	romain_lettuce     = {'cal':17, 'fat':0,   'carbs':3,   'prot':1,   'amount':'100g'}  
+	green_leaf_lettuce = {'cal':15, 'fat':0,   'carbs':3,   'prot':1,   'amount':'100g'}
+	green_beans        = {'cal':15, 'fat':0,   'carbs':3.5, 'prot':1,   'amount':'50g'}
+	pinto_beans        = {'cal':60, 'fat':0,   'carbs':22,  'prot':7,   'amount':'1/4drycup'}  
+	pea_pods           = {'cal':42, 'fat':0.2, 'carbs':7.8, 'prot':2.8, 'amount':'100g'}
+	red_pepper_bell    = {'cal':15, 'fat':0,   'carbs':3,   'prot':0,   'amount':'50g'}
+	eggplant           = {'cal':25, 'fat':0.2, 'carbs':6,   'prot':1,   'amount':'100g'}
+	cherry_tomato      = {'cal':18, 'fat':0.2, 'carbs':3.9, 'prot':0.9, 'amount':'100g'}
+	tomato_red         = {'cal':32, 'fat':0,   'carbs':7,   'prot':2,   'amount':'1cupchopped'}
 	return eval(ingredient)
 
 def calc_macros(ingredients):
@@ -74,7 +80,7 @@ if __name__ == '__main__':
 	print ("\n")
 	## BREAKFAST
 	print(colored("Breakfast:",'blue'))
-	breakfast = calc_macros(['westsoy_tofu','flax_oil','romain_lettuce','romain_lettuce'])
+	breakfast = calc_macros(['banyan_tofu','flax_oil','carrots','romain_lettuce','romain_lettuce'])
 	print_totals(breakfast,False)
 	## MORNING SNACK
 	print(colored("Morning Snack:","blue"))
@@ -82,15 +88,15 @@ if __name__ == '__main__':
 	print_totals(morning_snack,False)
 	## LUNCH
 	print(colored("Lunch:","blue"))
-	lunch = calc_macros(['romain_lettuce','flax_oil','carrots','tomato_red','brown_rice_dry','now_pea_protein'])
+	lunch = calc_macros(['romain_lettuce','romain_lettuce','flax_oil','rolled_oats','banyan_tofu'])
 	print_totals(lunch,False)
 	## AFTERNOON SNACK (near workout time)
 	print(colored("Afternoon snack:","blue"))
-	afternoon_snack = calc_macros(['coconut_water_MP','vega_smoothie'])
+	afternoon_snack = calc_macros(['romain_lettuce','romain_lettuce','nutritional_yeast','acorn_squash','acorn_squash','orange'])
 	print_totals(afternoon_snack,False)
 	## DINNER
 	print(colored("Dinner:","blue"))
-	dinner = calc_macros(['brown_rice_dry','brown_rice_dry','flax_oil','westsoy_tofu','performance_protein'])
+	dinner = calc_macros(['brown_rice_dry','flax_oil','flax_oil','flax_oil','now_pea_protein','performance_protein','acorn_squash','acorn_squash'])
 	print_totals(dinner,False)
 	## DAY TOTALS
 	day_total = day_totals()
